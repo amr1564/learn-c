acb19bb8979faec1aec855addef78b88c5fb3b82
Andrew Rivera <andrewmarcrivera@gmail.com>
2021-04-07 20:04:35 -0400

    Character manipulation practice

--- temperature/temperature.c
+++ temperature/temperature.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table
-    for fahr = 0, 20, ..., 300 */
 int main()
 {
-    int fahr, celsius;
+    char fHeader = 'F';
+    char cHeader = 'C';
+    printf("%2c%10c\n",fHeader, cHeader);
+
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;
@@ -13,8 +15,16 @@ int main()
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
+    }
+
+    printf("\n%2c%10c\n",cHeader, fHeader);
+    celsius = 0;
+    while (celsius <= upper) {
+        fahr = (9.0/5.0) * celsius + 32;
+        printf("%3.0f\t%6.1f\n", celsius, fahr);
+        celsius = celsius + step;
     }
 }
